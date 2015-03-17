14661a08ab7c711138ae46c223836f02020031ab
palmert <palm0116@algonquinlive.com>
2015-03-16 23:19:31 -0400

    Initial commit.

---
+++ app/src/main/java/co/palmer/bluetoothaccelerometer/viewmodels/BluetoothViewModel.java
@@ -0,0 +1,11 @@
+package co.palmer.bluetoothaccelerometer.viewmodels;
+
+import android.bluetooth.BluetoothAdapter;
+
+/**
+ * File created by Thom Palmer on 2015-03-16.
+ */
+public class BluetoothViewModel {
+    private BluetoothAdapter bluetoothAdapter;
+    
+}
